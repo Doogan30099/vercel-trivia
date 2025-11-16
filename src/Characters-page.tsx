541b0235d07b6de106012b03fcb24800b3871542
Patrick Nunes <pj.nunes.dy@gmail.com>
2025-11-15 21:36:41 -0500

    adde test

---
+++ src/Characters-page.tsx
@@ -0,0 +1,34 @@
+import { Container, Card, Col, Row } from "react-bootstrap";
+import { useCharacters } from "../hooks/useCharacters";
+import { Link } from "react-router-dom";
+
+const CharactersPage: React.FC = () => {
+  const { error, data, loading } = useCharacters();
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <Container>
+      <h1>Characters page, won't deploy since it doesn't pass the tests</h1>
+      <Row>
+        {data.characters.results.map((character: any) => {
+          return (
+            <Col>
+              <Link to={`/${character.id}`}>
+                <Card id={character.id} style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src={character.image} />
+                  <Card.Body>
+                    <Card.Title>{character.name}</Card.Title>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
+  );
+};
+
+export default CharactersPage;
